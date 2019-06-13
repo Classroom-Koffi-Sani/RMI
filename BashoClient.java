98d9db3d966dcb386e8dd4572f35565e3d53f5ed
koffisani <koffisani@gmail.com>
2019-06-13 09:40:58 +0000

    Ajout du client

---
+++ BashoClient.java
@@ -0,0 +1,20 @@
+import java.rmi.*;
+
+public class BashoClient {
+    public static void main(String args[]) {
+        try{
+            System.setProperty("java.security.policy", "/home/koffisani/Documents/RMI/security");
+            System.setSecurityManager(new RMISecurityManager());
+        } catch (Exception e) {
+            System.out.println("Security Exception " + e);
+        }
+        try {
+            DoueDeParole b = (DoueDeParole) Naming.lookup("rmi://127.0.0.1:1099/MatsuoBasho");
+            String message = b.parler();
+            System.out.println("BashoClient : " + message);
+        } catch (Exception e) {
+            System.out.println("Exception dans BashoClient.main" + e);
+            e.printStackTrace();
+        }
+    }
+}
